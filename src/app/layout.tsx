c490586d43c831e73a8ae0e098c1df99329f9542
Charlie Zhang <charliezhang2004@hotmail.com>
2025-11-22 20:14:40 +0000

    testing analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -10,7 +11,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Charlie Zhang | Portfolio",
+  title: "Charlie Z hang | Portfolio",
   description:
     "Hello! I'm Charlie Zhang, a UCD CS student driven by curiosity and a love for technology.",
 };
@@ -22,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <body className={`${poppins.variable} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
