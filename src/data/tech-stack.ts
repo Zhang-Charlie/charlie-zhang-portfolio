a9d3a6b5d917878e187f9212c198d2066024ac94
Charlie Zhang <charliezhang2004@hotmail.com>
2025-12-07 01:02:01 +0000

    Updates sql logo

--- src/data/tech-stack.ts
+++ src/data/tech-stack.ts
@@ -6,7 +6,7 @@ export const techStack: LogoItem[] = [
   { name: "Python", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
   { name: "HTML5", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" },
   { name: "CSS3", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" },
-  { name: "SQL", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" },
+  { name: "SQL", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" },
   { name: "Scheme", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/racket/racket-original.svg" },
   { name: "x86 Assembly", src: "/icons/x86.svg", className: "h-14 w-14" },
   { name: "Next.js", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" },
